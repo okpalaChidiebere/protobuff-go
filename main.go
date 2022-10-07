0632416f995e4b41fc2f536b8343578201a4af7e
okpalaChidiebere <okpalacollins4@gmail.com>
2022-10-06 20:21:31 -0600

    Write an application with JSON in go
    we learned that that employees json is about 122 bytes after checking the file size using `ls -lh`

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Employee struct {
+	Id          int32 `json:"id"`
+	Name        string `json:"name"`
+	Salary int `json:"salary"`
+}
 
 func main() {
-	fmt.Println("Hello, 世界")
+	employees := []Employee{}
+
+	employees = append(employees, Employee{
+		Name: "Hussein",
+		Salary: 1001,
+		Id: 1,
+	})
+
+	ahmed := Employee{
+		Name: "Ahmed",
+		Salary: 9000,
+		Id: 1002,
+	}
+
+	employees = append(employees, ahmed)
+
+	employees = append(employees, Employee{
+		Name: "Rick",
+		Salary: 5000,
+		Id: 1003,
+	})
+
+	body, _ := json.Marshal(employees)
+
+	fmt.Println(string(body)) //prints: [{"id":1,"name":"Hussein","salary":1001},{"id":1002,"name":"Ahmed","salary":9000},{"id":1003,"name":"Rick","salary":5000}]
+	ioutil.WriteFile("jsondata.json", body, 0666) //we write the file to hard drive and also set permissions on the file as well
 }
